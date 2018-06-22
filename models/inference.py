ef1090aee20a393fc0cc1ef397c0b7104974b981
vineelyettella <vineel.yettella@colorado.edu>
2018-06-22 08:11:26 -0700

    successfully running inference with RetinaNet

--- models/inference.py
+++ models/inference.py
@@ -8,8 +8,7 @@ import pandas as pd
 import pickle
 
 # import keras
-import keras
-
+from keras import backend
 
 # import keras_retinanet
 from keras_retinanet import models
@@ -23,6 +22,7 @@ import cv2
 import os
 import numpy as np
 import time
+from PIL import Image
 
 # set tf backend to allow memory to grow, instead of claiming everything
 import tensorflow as tf
@@ -41,17 +41,22 @@ def extract_bounded_objects(unscaled_image, boxes, scores, score_threshold=0.5):
         objects.append(unscaled_image[b[1]:b[3], b[0]:b[2], :])
     return objects
 
-def tree_extractor(image_folder, detector='resnet50_csv_50_inference.h5'):
-
-    keras.backend.tensorflow_backend.set_session(get_session())
+def tree_extractor(image_folder, detector='resnet50_csv_50_inference.h5', score_threshold=0.5):
+    backend.tensorflow_backend.set_session(get_session())
     model = models.load_model('./saved_models/' + detector, backbone_name='resnet50')
-
     types = ('*.jpg', '*.png', '*.jpeg', '*.JPG', '*.PNG')
     files = []
     for type in types:
-        files.extend(glob.glob(type))
+        files.extend(glob.glob(image_folder + '/' + type))
 
     for file in files:
+        print(file)
+
+        prefix = file.split('.')[0]
+        extension = file.split('.')[1]
+        prefix1 = prefix.split('/')[0]
+        prefix2 = prefix.split('/')[1]
+
         image = read_image_bgr(file)
         image = preprocess_image(image)
         image, scale = resize_image(image)
@@ -67,23 +72,26 @@ def tree_extractor(image_folder, detector='resnet50_csv_50_inference.h5'):
         boxes /= scale
 
         tree_images = extract_bounded_objects(unscaled_image, boxes, scores, 0.5)
-        pickle.dump({"tree_images": tree_images, "image": unscaled_image, "boxes": boxes, "scores": scores, "score_threshold": score_threshold}, open(file + '_inference.p', 'rb'))
+        valid_boxes = [box for i, box in enumerate(boxes[0]) if scores[0][i] > 0.5]
+
+        with open(image_folder + '/' + prefix2 + '_inference.p', 'wb') as handle:
+            pickle.dump({"tree_images": tree_images, "image": unscaled_image, "boxes": valid_boxes, "score_threshold": score_threshold}, handle)
 
 def parse_args(args):
     parser = argparse.ArgumentParser(description='Script for running inference on images.')
-    parser.add_argument('folder_in', help='Folder containing images to run inference on.')
-    parser.add_argument('--detector', help='Model for tree detection.')
+    parser.add_argument('--dir', help='Folder containing images to run inference on.')
+    parser.add_argument('--detector', help='Model for tree detection.', default='resnet50_csv_50_inference.h5')
     #parser.add_argument('--classifier', help ='Model for species classification.')
-
     return parser.parse_args(args)
 
 
 def main(args=None):
 
-    # parse arguments
-    if args is None:
-        args = sys.argv[:1]
-    args = parse_args(args)
 
+    args = sys.argv[1:]
+    args = parse_args(args)
     # extract trees
-    tree_extractor(image_folder=args.folder_in, detector='args.detector')
+    tree_extractor(image_folder=args.dir, detector=args.detector)
+
+if __name__ == '__main__':
+    main()
